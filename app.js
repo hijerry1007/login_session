4a5d3609058aa06a430f29a202f2e2f6bcb6c6ee
Jerry Lai <jerrydavid123@hotmail.com>
2020-02-25 23:45:38 +0800

    add session function to keep the status of users

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
-const port = 3000
 const bodyParser = require('body-parser')
 const verifyAccount = require('./verifyAccount')
+const app = express()
+const port = 3000
+const session = require('express-session')
 
 // set template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -12,11 +13,30 @@ app.set('view engine', 'handlebars')
 // set body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// set session
+app.use(session({
+  secret: 'copyCat',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 5,
+  },
+}));
+
 // set express static 
 app.use(express.static('public'))
 
+// set routes
 app.get('/', (req, res) => {
-  res.render('index')
+  console.log(req.session)
+  console.log(session.userName)
+  if (req.session.userName) {
+    res.render('verify', { userName: req.session.userName })
+  }
+  else {
+    res.render('index')
+  }
+
 })
 
 app.post('/', (req, res) => {
@@ -25,12 +45,19 @@ app.post('/', (req, res) => {
   if (loginStatus === 'verified') {
     const user = verifyAccount(req.body).verifiedAcct
     const userName = user[0].firstName
-    res.render('verify', { userName: userName })
+    req.session.userName = userName
+    res.render('verify', { userName })
+  }
+  else {
+    res.render('index', { errorMsg })
   }
 
-  res.render('index', { errorMsg: errorMsg })
 })
 
+app.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
 app.listen(port, () => {
   console.log(`The server is running on localhost: ${port}`)
 })
